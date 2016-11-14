44d265f638c6fd3869780f4b28c6c1c5f8af11ac
Ankur reyes <Ankur reyes>
2016-11-15 00:45:25 +0530

    awesome commit

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var path = require('path');
 var app = express();
 var multer  = require('multer');
 var request = require('request');
+var mongodb = require('mongodb');
+var moment = require('moment');
+
+var MongoClient = mongodb.MongoClient;
+
+var mongo_url =  process.env.MONGOLAB_URI || "mongodb://ankur1163:lightbulb1@ds013946.mlab.com:13946/ank1163";
 
 var Bing = require('node-bing-api')
             ({
@@ -20,20 +26,40 @@ app.listen(process.env.PORT || 5000);
 
 app.get("/api/imagesearch/:id",function(req,response){
       var f = req.params.id;
-      var t = req.query.offset;
+      var t = parseInt(req.query.offset);
+      var tj = req.query.offset;
       console.log("value of offset ",t);
       var g;
       var k;
+      var time = moment().format('MMMM Do YYYY, h:mm:ss a');
       var  l;
       var tarray =[];
+  //inserting search query in database
+  MongoClient.connect(mongo_url, function (err, db) {
+      // do inserts and quering here ...
+      if(err) {
+        throw err
+      }
+      var data = {
+          recentSearch: f,
+          when:time
+
+      }
+      var insertedId = db.collection('latestsearches').insert(data); // this is sync version of insertion
+
+
+  });
+
+  //insertion ends
 
       // start
 
       if(t===1){
+
         Bing.images(f, {top:10, skip: 0}, function(error, res, body){
           console.log("started");
-          g = JSON.stringify(body);
-          for(var i =0;i<9;i++)
+          //g = JSON.stringify(body);
+          for(var i =0;i<10;i++)
           {
             tarray.push({
             url: body.value[i].contentUrl,
@@ -43,8 +69,8 @@ app.get("/api/imagesearch/:id",function(req,response){
                         });
 
           }
-
-          response.json(tarray);
+           var mn = JSON.stringify(tarray)
+          response.send(tarray);
           console.log(tarray);
         })
       }
@@ -52,8 +78,10 @@ app.get("/api/imagesearch/:id",function(req,response){
         else if (t===2){
           Bing.images(f, {top:20, skip: 10}, function(error, res, body){
             console.log("started");
+            console.log("this is first body    \n",body);
             g = JSON.stringify(body);
-            for(var i =0;i<9;i++)
+            console.log("this is end of the body \n y\n e\n s \n \n \n \n \n \n \n \n")
+            for(var i =0;i<20;i++)
             {
               tarray.push({
               url: body.value[i].contentUrl,
@@ -63,6 +91,14 @@ app.get("/api/imagesearch/:id",function(req,response){
           });
 
             }
+
+           if (tarray.length > 10)
+           {
+
+           tarray = tarray.slice(-10);
+         }
+
+               console.log("this is final array ",tarray);
             response.json(tarray);
           })
 
@@ -72,7 +108,7 @@ app.get("/api/imagesearch/:id",function(req,response){
           Bing.images(f, {top:30, skip: 20}, function(error, res, body){
             console.log("started");
             g = JSON.stringify(body);
-            for(var i =0;i<9;i++)
+            for(var i =0;i<30;i++)
             {
               tarray.push({
               url: body.value[i].contentUrl,
@@ -82,6 +118,11 @@ app.get("/api/imagesearch/:id",function(req,response){
           });
 
             }
+
+                       if (tarray.length > 10)
+                       {
+                       tarray = tarray.slice(-10);
+                     }
             response.json(tarray);
           })
 
@@ -90,8 +131,9 @@ app.get("/api/imagesearch/:id",function(req,response){
           response.send("No results to display. Please put offset=1,2 or 3. Api doesnt understand your offset value")
         }
 
+var fjson = JSON.stringify(tarray);
 
-       console.log(tarray);
+       console.log(fjson);
 
 
       })
@@ -106,3 +148,19 @@ app.get("/api/imagesearch/:id",function(req,response){
 app.get("/",function(req,res){
   res.render("index.html");
 })
+//this starts
+
+app.get('/api/latest/imagesearch/', function(req, res){
+    MongoClient.connect(mongo_url, function (err, db) {
+        // do inserts and quering here ...
+        if(err) {
+          throw err
+        }
+      //var latsearches =db.collection('latestsearches').find( );
+      var documents = SpeCollection.AsQueryable();
+      console.log("latest searches are ",latsearches);
+
+
+
+    });
+});
